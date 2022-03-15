5e550904893cdb7fce1a2ab45c15fa3ef9a173f3
alexey <kudimov20000@mail.ru>
2022-03-15 14:28:54 +0300

    DZ

---
+++ django_tutorial/app_tutorial/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('index/', views.index, name='index'),
+    path('book_detail/<int:book_id>/', views.book_detail, name='book_detail'),
+    path('book_list/', views.book_list, name='book_list'),
+    path('review_detail/<int:review_id>/', views.review_detail, name='review_detail'),
+    path('review_list/', views.review_list, name='review_list'),
+    path('book_review_list/<int:book_id>/', views.book_review_list, name='book_review_list'),
+]
+
